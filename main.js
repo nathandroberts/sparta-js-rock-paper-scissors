53acfdbae9e441f62972217e195910f4a904d2e5
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-10-31 22:25:54 +0000

    game finished

--- main.js
+++ main.js
@@ -3,29 +3,33 @@
 // function for computer's choice
 function randomChoice(){
   var choice = Math.floor(Math.random() * 3 );
-  if (choice = 0) {
+  if (choice === 0) {
     return 'r';
-  } else if (choice = 1) {
+  } else if (choice === 1) {
     return 'p';
-  } else if (choice = 2) {
+  } else if (choice === 2) {
     return 's';
   }
 }
 //score
 var playerScore = 0;
 var cpuScore =0;
-
+//game
 function game(player, cpu) {
-  var player = prompt('enter rock (r), paper (p) or scissors (s)');
   var cpu = randomChoice();
+  //player won
   while (playerScore === 3) {
     alert('Game finished. Player won.');
     break;
-  } while (cpuScore === 3) {
+  }
+  //cpu won
+  while (cpuScore === 3) {
     alert('Game finished. CPU won');
     break;
   }
+  //game code
   while (playerScore < 3 && cpuScore < 3) {
+    var player = prompt('enter rock (r), paper (p) or scissors (s)');
     if (player === 'r' && cpu === 'r') {
       alert('player:rock, cpu: rock. TIE')
       game();
