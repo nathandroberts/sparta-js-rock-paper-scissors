9455f17a53b86022c469dae01c8259ce954616cb
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-11-01 11:00:28 +0000

    group solution test. getUserInput() function created

---
+++ rps-solution/main.js
@@ -0,0 +1,20 @@
+// get input
+
+
+function getUserInput() {
+  prompt("Choose rock (r), paper (p) or scissors (s)");
+
+}
+getUserInput()
+// get player move
+
+
+// random play
+
+
+// get computer move
+
+
+// will need to play to 5
+
+// check for winner
